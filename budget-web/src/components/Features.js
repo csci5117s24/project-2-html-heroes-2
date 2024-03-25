5170f8e41f3f5e0daada3ef16ea5583fcb6e9c08
Connor Holm <50306883+connorholm@users.noreply.github.com>
2024-03-25 12:26:54 -0500

    updated text color

--- budget-web/src/components/Features.js
+++ budget-web/src/components/Features.js
@@ -5,7 +5,7 @@ function Features() {
     return (
         <>
 
-            <section class="text-white text-slate-800">
+            <section class="text-white text-black">
                 <div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16">
                     <div class="mx-auto max-w-lg text-center">
                     <h2 class="text-3xl font-bold sm:text-4xl">Kickstart your marketing</h2>
