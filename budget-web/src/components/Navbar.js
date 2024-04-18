878848535d6cdfe68946974c0df06511edd45278
Daqing Chen <chen7751@umn.edu>
2024-04-18 01:08:03 -0500

    add log-in/log-out

--- budget-web/src/components/Navbar.js
+++ budget-web/src/components/Navbar.js
@@ -1,42 +1,102 @@
 // https://tailwindcomponents.com/component/responsive-tailwind-css-navbar-2
+import React, { useEffect, useState } from 'react';
+
 function Navbar() {
-    return (
+    const [user1, setUser] = useState(null);
 
-        <header class="bg-gray-800">
-        <nav class="container mx-auto px-6 py-3">
-            <div class="flex items-center justify-between">
-            <div class="text-white font-bold text-xl">
-                <a href="/">Logo</a>
-            </div>
-            <div class="hidden md:block">
-                <ul class="flex items-center space-x-8">
-                <li><a href="/" class="text-white">Home</a></li>
-                <li><a href="/" class="text-white">About</a></li>
-                <li><a href="/" class="text-white">Services</a></li>
-                <li><a href="/" class="text-white">Contact</a></li>
-                </ul>
-            </div>
-            <div class="md:hidden">
-                <button class="outline-none mobile-menu-button">
-                <svg class="w-6 h-6 text-white" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                    <path d="M4 6h16M4 12h16M4 18h16"></path>
-                </svg>
-                </button>
-            </div>
-            </div>
-            <div class="mobile-menu hidden md:hidden">
-            <ul class="mt-4 space-y-4">
-                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Home</a></li>
-                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">About</a></li>
-                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Services</a></li>
-                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Contact</a></li>
-            </ul>
-            </div>
-            
-        </nav>
-        </header>
+    async function fetchAuthDetails() {
+        fetch('/.auth/me')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                if (data != null) {
+                    setUser(data?.clientPrincipal.userId);
+                }
+            })
+            .catch(function (err) {
+                console.error('Error fetching auth details:', err);
+            });
+    }
 
-    )
+    useEffect(() => {
+        fetchAuthDetails();
+    }, [user1]);
+
+    if (user1 === null) {
+
+        return (
+            <header class="bg-gray-800">
+                <nav class="container mx-auto px-6 py-3">
+                    <div class="flex items-center justify-between">
+                        <div class="text-white font-bold text-xl">
+                            <a href="/">Logo</a>
+                        </div>
+                        <div class="hidden md:block">
+                            <ul class="flex items-center space-x-8">
+                                <li><a href="/.auth/login/github" class="text-white">Log in</a></li>
+                            </ul>
+                        </div>
+                        <div class="md:hidden">
+                            <button class="outline-none mobile-menu-button">
+                                <svg class="w-6 h-6 text-white" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path d="M4 6h16M4 12h16M4 18h16"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="mobile-menu hidden md:hidden">
+                        <ul class="mt-4 space-y-4">
+                            <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Home</a></li>
+                            <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">About</a></li>
+                            <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Services</a></li>
+                            <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Contact</a></li>
+                        </ul>
+                    </div>
+
+                </nav>
+            </header>
+
+        )
+    } else {
+
+        return (
+            <>
+                <header class="bg-gray-800">
+                    <nav class="container mx-auto px-6 py-3">
+                        <div class="flex items-center justify-between">
+                            <div class="text-white font-bold text-xl">
+                                <a href="/">Logo</a>
+                            </div>
+                            <div class="hidden md:block">
+                                <ul class="flex items-center space-x-8">
+                                    <li><a href="/" class="text-white">Dashboard</a></li>
+                                    <li><a href="/" class="text-white">Advanced Spending Summary</a></li>
+                                    <li><a href="/" class="text-white">Set up your budget plan</a></li>
+                                    <li><a href="/.auth/logout" class="text-white">Log out</a></li>
+                                </ul>
+                            </div>
+                            <div class="md:hidden">
+                                <button class="outline-none mobile-menu-button">
+                                    <svg class="w-6 h-6 text-white" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                                        <path d="M4 6h16M4 12h16M4 18h16"></path>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
+                        <div class="mobile-menu hidden md:hidden">
+                            <ul class="mt-4 space-y-4">
+                                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Home</a></li>
+                                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">About</a></li>
+                                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Services</a></li>
+                                <li><a href="/" class="block px-4 py-2 text-white bg-gray-900 rounded">Contact</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                </header>
+            </>
+        )
+    }
 }
 
 export default Navbar;
--- budget-web/src/pages/Landing.js
+++ budget-web/src/pages/Landing.js
@@ -1,20 +1,73 @@
 import Features from "../components/Features";
 import Hero from "../components/Hero";
 import SplitImageContent from "../components/SplitImageContent";
+import React, { useEffect, useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 
 function Landing() {
-    return (
-        <>
-            <div className="">
-                <Hero />
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                    <path fill="#111827" fill-opacity="1" d="M0,320L48,277.3C96,235,192,149,288,138.7C384,128,480,192,576,181.3C672,171,768,85,864,69.3C960,53,1056,107,1152,112C1248,117,1344,75,1392,53.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
-                </svg>
-                <Features />
-                <SplitImageContent />
-            </div>
-        </>
-    )
-}
+    const [user1, setUser] = useState(null);
+
+
+    async function fetchAuthDetails() {
+        fetch('/.auth/me')
+            .then(function (response) {
+                return response.json();
+            })
+            .then(async function (data) {
+                if (data.clientPrincipal != null) {
+                    console.log(data.clientPrincipal.userId);
+                    setUser(data.clientPrincipal.userId)
+                    // see if the user is in the database
+                    const result1 = await fetch("/api/user/", {
+                        method: "get",
+                      });
+                    const user1 = await result1.json()
+                    console.log(user1)
+                    
+                    //if use isn't in the database, then we add it into database
+                    if(user1.user === null){
+                        const user_add = await fetch("/api/user", {
+                            method: "PUT",
+                            headers: {
+                              "Content-Type": "application/json",
+                            },
+                            body: JSON.stringify()
+                          })
+                          if (user_add.ok) {
+                            console.log("id: " + await user_add.json());
+                          } else {
+                            console.log("failed")
+                          }
+                    }
+                }else{
+                    console.log(data)
+                }
+            })
+            .catch(function (err) {
+                console.error('Error fetching auth details:', err);
+            });
+    }
+
+    useEffect(() => {
+        fetchAuthDetails();
+    }, [user1]);
+
+    if (user1 === null) {
+        return (
+          <div>
+            <Hero />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+              <path fill="#111827" fillOpacity="1" d="M0,320L48,277.3C96,235,192,149,288,138.7C384,128,480,192,576,181.3C672,171,768,85,864,69.3C960,53,1056,107,1152,112C1248,117,1344,75,1392,53.3L1440,32L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"></path>
+            </svg>
+            <Features />
+            <SplitImageContent />
+          </div>
+        );
+      } else {
+        return <Navigate to="/dashboard_tyler" replace />;
+      }
+};
+
 
 export default Landing;
