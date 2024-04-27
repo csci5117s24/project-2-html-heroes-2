58a31755a4b495c7a3feb605a34499f2abc50a58
Hads23 <kotif@umn.edu>
2024-04-27 12:19:30 -0500

    added total transactions sum to transactions page

--- budget-web/src/pages/TransactionList.js
+++ budget-web/src/pages/TransactionList.js
@@ -1,6 +1,21 @@
 import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 
+/* calculate total costs from a list of transactions i.e  [{...,"value" : "56.70", ... }, ...] 
+   returns a double with the summation of all transaction values within the list with two decimal places*/
+function transactionTotal(transactionList) {
+  let sum = 0;
+  for (let i = 0; i < transactionList.length; i++) {
+    sum += parseInt(transactionList[i].value);
+  }
+  return sum.toFixed(2);
+}
+
+
+
+
+
+
 export const TransactionList = () => {
   const [transactions, setTransactions] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -61,24 +76,45 @@ export const TransactionList = () => {
         ))}
       </div>
       {transactions.length > 0 ? (
-        <ul className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          {transactions.map((transaction) => (
-            <li key={transaction._id} className="mb-4">
-              <div className="flex justify-between items-center">
-                <div>
-                  <p className="text-gray-700 font-bold">
-                    {transaction.description}
-                  </p>
-                  <p className="text-gray-600">{transaction.category}</p>
-                  <p className="text-gray-500">{transaction.date}</p>
+        <div>
+          <ul className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            {transactions.map((transaction) => (
+              <li key={transaction._id} className="mb-4">
+                <div className="flex justify-between items-center">
+                  <div>
+                    <p className="text-gray-700 font-bold">
+                      {transaction.description}
+                    </p>
+                    <p className="text-gray-600">{transaction.category}</p>
+                    <p className="text-gray-500">{transaction.date}</p>
+                  </div>
+                  <div>
+                    <p className="text-2xl font-bold">${transaction.value}</p>
+                  </div>
                 </div>
-                <div>
-                  <p className="text-2xl font-bold">${transaction.value}</p>
-                </div>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="flex justify-between items-center">
+              <div>
+                <p className="text-2xl font-bold">
+                  Total Expenses:
+                </p>
+                {categoryName === undefined ? (
+                  <p className="text-gray-600">All</p>
+                 ) : ( 
+                  <p className="text-gray-600">{categoryName}</p>
+                 )}
               </div>
-            </li>
-          ))}
-        </ul>
+              <div>
+                <p className="text-2xl font-bold">${transactionTotal(transactions)}</p>
+              </div>
+            </div>
+          </ul>
+
+        </div>
       ) : (
         <p className="text-gray-700">No transactions found.</p>
       )}
