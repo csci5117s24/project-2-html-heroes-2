3e1e65dafe46b63e3e49b5af6facab8d1b9903ef
Hads23 <kotif@umn.edu>
2024-04-27 14:02:17 -0500

    Made budgetManagement input field more rigourous and added $ to inputs, decimals still work like jank

--- budget-web/src/pages/SetUpBudget.js
+++ budget-web/src/pages/SetUpBudget.js
@@ -5,9 +5,45 @@ export default function SetUpBudget() {
     const [monthlyIncome, setMonthlyIncome] = useState(0);
     const [savingsTarget, setSavingsTarget] = useState(0);
 
+    /*  set monthlyIncomt input field 
+        max input value of 999,999,999,999
+        no negative inputs
+        sets savings to be less than monthly income
+        */
+    function handleSetMonthlyIncome2(e) {
+        let inputVal = parseFloat(e.target.value)
+        let min = parseInt(savingsTarget)
+        let max = 999999999999
+        if (inputVal < 0 || undefined){ // set monthlyIncome to 0 if negative or undefined
+            inputVal = 0;
+        }
+        if (inputVal < min) {
+            setSavingsTarget(inputVal); // set savings to be less than monthly income
+        }
+        if (inputVal > max) {           // set monthly income to max if too big
+            inputVal = max;
+        }
+        setMonthlyIncome(inputVal)
+    }
+
+    /*  setsavingsAmount from input field
+        ensure savings is no larger than monthly income / set savings to monthly income
+        ensure savings is not less than 0 / set savings to 0
+        */
+    function handleSetSavingsTarget2(e) {
+        let inputVal = parseFloat(e.target.value)
+        let max = parseFloat(monthlyIncome)
+        if (inputVal > max) {
+            inputVal = max
+        } else if (inputVal < 0){
+            inputVal = 0;
+        }
+        setSavingsTarget(inputVal);
+    }
+
     const handleSave = async () => {
-        if (monthlyBudget < 0) {
-            window.alert("Your Budget should be positive")
+        if (monthlyBudget < 0 || isNaN(monthlyBudget)) {
+            window.alert("Your Budget should be positive and not NaN")
         } else {
             await fetch("/api/budget/"+ encodeURIComponent(monthlyBudget), {
                 method: "POST",
@@ -26,7 +62,7 @@ export default function SetUpBudget() {
 
     const navigate = useNavigate();
 
-    const monthlyBudget = monthlyIncome && savingsTarget
+    let monthlyBudget = monthlyIncome && savingsTarget
         ? monthlyIncome - savingsTarget
         : 0;
     
@@ -34,6 +70,14 @@ export default function SetUpBudget() {
     ? savingsTarget / monthlyIncome * 100
     : 0;
 
+    // ensure Saving Numbers look correct (not NaN and two decimal places)
+    let savings;
+    if (isNaN(savingsTarget)) {
+        savings = 0;
+    } else {
+        savings = savingsTarget;
+    }
+    savings = parseInt(savings).toFixed(2)
 
     return (
         <>
@@ -56,26 +100,35 @@ export default function SetUpBudget() {
                             <div class="flex mb-6">
                                 <div>
                                     <p class="mb-2 flex justify-center text-xl tracking-tight text-gray-900 dark:text-white">Monthly income</p>
-                                    <input
-                                        class="dark:bg-gray-800 w-64 mx-auto text-center text-3xl font-bold flex justify-center dark:text-white border-b border-gray-400 border-b-1 appearance-none w-full text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
-                                        type="text"
-                                        name="description"
-                                        onChange={(e) => setMonthlyIncome(e.target.value)}
-                                        placeholder=""
-                                    />
+                                    <div class="flex mb-6 justify-center">
+                                        <p class="dark:bg-gray-800 text-3xl font-bold dark:text-white border-b border-gray-400 border-b-1 appearance-none text-gray-900 leading-tight focus:outline-none focus:shadow-outline">$</p>
+                                        <input
+                                            class="dark:bg-gray-800 w-64 mx-auto text-center text-3xl font-bold flex justify-center dark:text-white border-b border-gray-400 border-b-1 appearance-none w-full text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
+                                            type="number"
+                                            name="description"
+                                            onChange={handleSetMonthlyIncome2}
+                                            value={monthlyIncome}
+                                            placeholder=""
+                                        />
+                                    </div>
+                                
                                 </div>
                                 <div>
                                     <p class="ml-9 mr-9 text-4xl tracking-tight text-gray-900 dark:text-white">-</p>
                                 </div>
                                 <div>
                                     <p class="mb-2 flex justify-center text-xl tracking-tight text-gray-900 dark:text-white">Savings target</p>
-                                    <input
-                                        class="dark:bg-gray-800 w-64 mx-auto text-center text-3xl font-bold flex justify-center dark:text-white border-b border-gray-400 border-b-1 appearance-none w-full text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
-                                        type="text"
-                                        name="description"
-                                        onChange={(e) => setSavingsTarget(e.target.value)}
-                                        placeholder=""
-                                    />
+                                    <div class="flex mb-6 justify-center">
+                                        <p class="dark:bg-gray-800 text-3xl font-bold dark:text-white border-b border-gray-400 border-b-1 appearance-none text-gray-900 leading-tight focus:outline-none focus:shadow-outline">$</p>
+                                        <input
+                                            class="dark:bg-gray-800 w-64 mx-auto text-center text-3xl font-bold flex justify-center dark:text-white border-b border-gray-400 border-b-1 appearance-none w-full text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
+                                            type="number"
+                                            name="description"
+                                            onChange={handleSetSavingsTarget2}
+                                            placeholder=""
+                                            value = {savingsTarget}
+                                        />
+                                    </div>
                                     <p class="mb-2 mt-2 flex justify-center text-l tracking-tight text-gray-200 dark:text-white">{Number(rate).toFixed(2)} % of your monthly income</p>
 
                                 </div>
@@ -88,7 +141,7 @@ export default function SetUpBudget() {
                                     <p class="mb-2 flex text-center justify-center text-xl tracking-tight text-gray-900 dark:text-white">Monthly budget</p>
                                     <p
                                         class="dark:bg-gray-800 w-64 mx-auto text-center text-3xl font-bold flex justify-center dark:text-white appearance-none text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
-                                    >{monthlyBudget}</p>
+                                    >${monthlyBudget}</p>
                                 </div>
                             </div>
                             <div class="flex justify-center">
@@ -110,17 +163,17 @@ export default function SetUpBudget() {
                     <div class="flex ml-0 justify-center mx-auto max-w-5xl py-6 sm:px-6 lg:px-8">
                         <div class="max-w-4xl ml-0 justify-center p-6 bg-white border border-gray-500 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                             <h1 class="mb-9 text-center text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Potential Savings</h1>
-                            <h2 class="mb-9 text-center text-xl tracking-tight text-gray-900 dark:text-white">If you set aside $ {savingsTarget} each month, you will get</h2>
+                            <h2 class="mb-9 text-center text-xl tracking-tight text-gray-900 dark:text-white">If you set aside $ {savings} each month, you will get</h2>
                             <div class="flex mb-6 justify-center">
-                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savingsTarget * 12}</p>
+                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savings * 12}</p>
                                 <p class="font-bold mb-2 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">in 1 year</p>
                             </div>
                             <div class="flex mb-6 justify-center">
-                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savingsTarget * 24}</p>
+                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savings * 24}</p>
                                 <p class="font-bold mb-2 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">in 2 years</p>
                             </div>
                             <div class="flex mb-6 justify-center">
-                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savingsTarget * 36}</p>
+                                <p class="font-bold mb-2 mr-9 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">$ {savings * 36}</p>
                                 <p class="font-bold mb-2 flex justify-center text-xl tracking-tight text-green-600 dark:text-green">in 3 years</p>
                             </div>
                         </div>
