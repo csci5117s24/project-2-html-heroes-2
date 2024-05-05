e87c91a66ce9cc39cdc21071cae2055be61cd5ae
Connor Holm <50306883+connorholm@users.noreply.github.com>
2024-05-05 17:40:52 -0500

    updates to footer and landing page

--- budget-web/src/components/Hero.js
+++ budget-web/src/components/Hero.js
@@ -20,30 +20,15 @@ function Hero() {
             </div>
             <div class="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-32 lg:flex lg:px-8 lg:py-40">
                 <div class="mx-auto max-w-2xl flex-shrink-0 lg:mx-0 lg:max-w-xl lg:pt-8">
-                <img class="h-11" src="https://isren.org/storage/isren-network.png" alt="Your Company" 
-                />
-                <div class="mt-24 sm:mt-32 lg:mt-16">
-                    <a href="/" class="inline-flex space-x-6">
-                    <span class="rounded-full bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-cyan-500 ring-1 ring-inset ring-indigo-500/20">What's new</span>
-                    <span class="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-300">
-                        <span>About us</span>
-                        <svg class="h-5 w-5 text-gray-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
-                        </svg>
-                    </span>
-                    </a>
-                </div>
-                <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">YOUR GATEWAY TO THE WORLD OF KNOWLEDGE</h1>
-                <p class="mt-6 text-lg leading-8 text-gray-300">Explore the depths of scientific and research work and stay informed of the latest developments in knowledge..</p>
-                <div class="mt-10 flex items-center gap-x-6">
-                    <a href="/" class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Get started</a>
-                    <a href="/" class="text-sm font-semibold leading-6 text-white">Learn more <span aria-hidden="true">→</span></a>
-                </div>
+                    <h1 class="mt-10 text-4xl font-bold tracking-tight text-white sm:text-6xl">MANAGE YOUR TRANSACTIONS EFFORTLESSLY</h1>
+                    <p class="mt-6 text-lg leading-8 text-gray-300">Explore how our budget tracker can manage any purchase and keep you within your customizable budget..</p>
+                    <div class="mt-10 flex items-center gap-x-6">
+                        <a href={".auth/login/github"} class="rounded-md bg-blue-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-400">Get started</a>
+                    </div>
                 </div>
                 <div class="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
                 <div class="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-                    <img src="https://spruko.com/demo/ren/demo/img/ltr-screens/screen2.jpg" alt="App screenshot" width="2432" height="1442" class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" 
-                    />
+                    <img src={"/dashboard.png"} alt="App screenshot" width="2432" height="1442" class="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10" />
                 </div>
                 </div>
             </div>
